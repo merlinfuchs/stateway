ae2b292f9996328b4439e2cc494dbe0f28db8ac5
merlinfuchs <merlinfuchs2001@gmail.com>
2025-11-13 17:06:50 +0100

    fix cache implementation

--- stateway-lib/cache/client.go
+++ stateway-lib/cache/client.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"encoding/json"
 
-	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/snowflake/v2"
 	"github.com/merlinfuchs/stateway/stateway-lib/broker"
 	"github.com/merlinfuchs/stateway/stateway-lib/service"
 )
+
+var _ Cache = &CacheClient{}
 
 type CacheClient struct {
 	b       broker.Broker
@@ -26,110 +27,110 @@ func (c *CacheClient) WithOptions(opts ...CacheOption) *CacheClient {
 	}
 }
 
-func (c *CacheClient) GetGuild(ctx context.Context, id snowflake.ID, opts ...CacheOption) (*discord.Guild, error) {
+func (c *CacheClient) GetGuild(ctx context.Context, id snowflake.ID, opts ...CacheOption) (*Guild, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[*discord.Guild](ctx, c.b, CacheMethodGetGuild, GuildGetRequest{
+	return cacheRequest[*Guild](ctx, c.b, CacheMethodGetGuild, GuildGetRequest{
 		GuildID: id,
 		Options: options,
 	})
 }
 
-func (c *CacheClient) GetGuilds(ctx context.Context, opts ...CacheOption) ([]*discord.Guild, error) {
+func (c *CacheClient) GetGuilds(ctx context.Context, opts ...CacheOption) ([]*Guild, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[[]*discord.Guild](ctx, c.b, CacheMethodListGuilds, GuildListRequest{
+	return cacheRequest[[]*Guild](ctx, c.b, CacheMethodListGuilds, GuildListRequest{
 		Options: options,
 	})
 }
 
-func (c *CacheClient) SearchGuilds(ctx context.Context, data json.RawMessage, opts ...CacheOption) ([]*discord.Guild, error) {
+func (c *CacheClient) SearchGuilds(ctx context.Context, data json.RawMessage, opts ...CacheOption) ([]*Guild, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[[]*discord.Guild](ctx, c.b, CacheMethodSearchGuilds, GuildSearchRequest{
+	return cacheRequest[[]*Guild](ctx, c.b, CacheMethodSearchGuilds, GuildSearchRequest{
 		Data:    data,
 		Options: options,
 	})
 }
 
-func (c *CacheClient) GetChannel(ctx context.Context, guildID snowflake.ID, channelID snowflake.ID, opts ...CacheOption) (*discord.Channel, error) {
+func (c *CacheClient) GetChannel(ctx context.Context, guildID snowflake.ID, channelID snowflake.ID, opts ...CacheOption) (*Channel, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[*discord.Channel](ctx, c.b, CacheMethodGetChannel, ChannelGetRequest{
+	return cacheRequest[*Channel](ctx, c.b, CacheMethodGetChannel, ChannelGetRequest{
 		GuildID:   guildID,
 		ChannelID: channelID,
 		Options:   options,
 	})
 }
 
-func (c *CacheClient) GetChannels(ctx context.Context, guildID snowflake.ID, opts ...CacheOption) ([]*discord.Channel, error) {
+func (c *CacheClient) GetChannels(ctx context.Context, guildID snowflake.ID, opts ...CacheOption) ([]*Channel, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[[]*discord.Channel](ctx, c.b, CacheMethodListChannels, ChannelListRequest{
+	return cacheRequest[[]*Channel](ctx, c.b, CacheMethodListChannels, ChannelListRequest{
 		GuildID: guildID,
 		Options: options,
 	})
 }
 
-func (c *CacheClient) SearchChannels(ctx context.Context, data json.RawMessage, opts ...CacheOption) ([]*discord.Channel, error) {
+func (c *CacheClient) SearchChannels(ctx context.Context, data json.RawMessage, opts ...CacheOption) ([]*Channel, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[[]*discord.Channel](ctx, c.b, CacheMethodSearchChannels, ChannelSearchRequest{
+	return cacheRequest[[]*Channel](ctx, c.b, CacheMethodSearchChannels, ChannelSearchRequest{
 		Data:    data,
 		Options: options,
 	})
 }
 
-func (c *CacheClient) GetRole(ctx context.Context, guildID snowflake.ID, roleID snowflake.ID, opts ...CacheOption) (*discord.Role, error) {
+func (c *CacheClient) GetRole(ctx context.Context, guildID snowflake.ID, roleID snowflake.ID, opts ...CacheOption) (*Role, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[*discord.Role](ctx, c.b, CacheMethodGetRole, RoleGetRequest{
+	return cacheRequest[*Role](ctx, c.b, CacheMethodGetRole, RoleGetRequest{
 		GuildID: guildID,
 		RoleID:  roleID,
 		Options: options,
 	})
 }
 
-func (c *CacheClient) GetRoles(ctx context.Context, guildID snowflake.ID, opts ...CacheOption) ([]*discord.Role, error) {
+func (c *CacheClient) GetRoles(ctx context.Context, guildID snowflake.ID, opts ...CacheOption) ([]*Role, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[[]*discord.Role](ctx, c.b, CacheMethodListRoles, RoleListRequest{
+	return cacheRequest[[]*Role](ctx, c.b, CacheMethodListRoles, RoleListRequest{
 		GuildID: guildID,
 		Options: options,
 	})
 }
 
-func (c *CacheClient) SearchRoles(ctx context.Context, data json.RawMessage, opts ...CacheOption) ([]*discord.Role, error) {
+func (c *CacheClient) SearchRoles(ctx context.Context, data json.RawMessage, opts ...CacheOption) ([]*Role, error) {
 	options := c.options
 	for _, opt := range opts {
 		opt(&options)
 	}
 
-	return cacheRequest[[]*discord.Role](ctx, c.b, CacheMethodSearchRoles, RoleSearchRequest{
+	return cacheRequest[[]*Role](ctx, c.b, CacheMethodSearchRoles, RoleSearchRequest{
 		Data:    data,
 		Options: options,
 	})
