b53fd3cfb216cd8a1bac533e246f3a85821cdc3b
Merlin <merlinfuchs2001@gmail.com>
2026-03-22 11:55:09 +0100

    log when app is disabled due to fatal error

--- stateway-gateway/app/helpers.go
+++ stateway-gateway/app/helpers.go
@@ -145,6 +145,14 @@ func (a *App) disableIfFatal(ctx context.Context, err error) bool {
 }
 
 func (a *App) disable(ctx context.Context, code gateway.AppDisabledCode, message string) {
+	slog.Warn(
+		"Disabling app due to fatal error",
+		slog.String("app_id", a.model.ID.String()),
+		slog.String("group_id", a.model.GroupID),
+		slog.String("code", string(code)),
+		slog.String("message", message),
+	)
+
 	err := a.appStore.DisableApp(ctx, store.DisableAppParams{
 		ID:              a.model.ID,
 		DisabledCode:    code,
